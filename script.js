1176d6b1c5d699045d638bcec7e2fcdcdce92b90
Evgeny Khodykine <evgeny.khodykin@yandex.ru>
2022-06-22 13:04:13 +0500

    bubble sort was added

--- script.js
+++ script.js
@@ -140,11 +140,18 @@ const comparationColor = (a, b) => {
 const sortAPI = {
   bubbleSort(arr, comparation) {
     // TODO: допишите функцию сортировки пузырьком
-    fruits = fruits.sort(comparation)
-    },
+    const n = arr.length
+    for (let i = 0; i < n-1; i++) {
+    for (let j = 0; j < n-1-i; j++) {
+      if (comparation(arr[j], arr[j+1])) {
+        let temp = arr[j+1] 
+           arr[j+1] = arr[j]
+           arr[j] = temp;   
+    }}}},
 
   quickSort(arr, comparation) {
     // TODO: допишите функцию быстрой сортировки
+    
   },
 
   // выполняет сортировку и производит замер времени
